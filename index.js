6a0f2d9519653877771536aafd85b37562ba4696
AntoniaG <antonia.grillmair@gmail.com>
2020-04-30 20:00:46 +0200

    frontend mit backend verbunden :)

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 
+const exphbs = require('express-handlebars');
+
 /**
  * App Variables
  */
@@ -17,27 +19,27 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-
 /**
  *  App Configuration
  */
-
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
+// Set up handelbars
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
-//app.set("views", path.join(__dirname, "views"));
-//app.set("view engine", "html");
-//app.use(express.static(path.join(__dirname, "public")));
+// setup css
+app.use(express.static('public'));
+
+
+// The index route
+app.get("/", (req, res) => res.render("home"));
 
 var cons = require('consolidate');
 
-// view engine setup
-app.engine('html', cons.swig)
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
 
 
 
@@ -50,10 +52,7 @@ const url =
 "mongodb+srv://VenzlMa:x2345678@gamepot-zzavq.mongodb.net/test?retryWrites=true&w=majority"
 let db;
 
-// The index route
-app.get("/", function(req, res) {
-   res.render("index", { title: "Home" });
-});
+
 
 // Connect to the database with [url]
 (async () => {
@@ -89,7 +88,7 @@ app.post("/players", async function(req, res) {
        await db.collection("players").insertOne({ username, credits });
        //console.log(`Created Player ${username}`);
        //res.send({ status: true, msg: "player created-> " + username + ", credits:" + credits});
-	   res.render("players", { title: "Home" })
+	   res.render("playersAG", { title: "Home" })
    }
 });
 
